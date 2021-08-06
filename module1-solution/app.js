17b919b29435239b00274ee18ffbdd5582bb8398
Tehreem <tehreem@github.com>
2021-08-07 00:05:22 +0500

    updated folder

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,7 +9,7 @@ function LunchCheckController($scope) {
   $scope.lunchmenu= " ";
   $scope.fooditems = 0;
   $scope.message = "Enter Elements";
-  $scope.color="green";
+  $scope.color="black";
 
   console.log(LunchCheckController);
 
@@ -18,12 +18,17 @@ function LunchCheckController($scope) {
     var total_items = t_items.trim();
     $scope.fooditems= total_items.split(",").length;
   //return t_items;
-    if ($scope.fooditems > 3) {
+    if ($scope.lunchmenu==" " || $scope.lunchmenu=="") {
+      $scope.message = "Must Enter Elements";
+      $scope.color="red";
+    }
+    else if ($scope.fooditems > 3) {
       $scope.message = "Too Much";
       $scope.color="red";
       }
     else if ($scope.fooditems < 4) {
       $scope.message="Enjoy!";
+      $scope.color="green";
     }
   }
 
